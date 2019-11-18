6e7f11a55294c6287c5fbfa84bfc296e598f7b67
tmoran18 <moran.tim01@gmail.com>
2019-11-18 20:36:35 +1000

    working on design - need to add delete functions

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 
 // TODO Controller
 var todoController = (function () {
-  // Private local Scope
 
+  // Function constructor for the todo's. This will make each todo enter an object.
+  var Todo = function (id, description) {
+    this.id = id;
+    this.description = description;
+  };
 
-  // Public global scope
+  // The array where the Todos will be stored.
+  var allTodos = [];
+
   return {
+    // Add the todo to the array
+    addTodo: function (description) {
+      var newTodo, ID;
 
-  }
+      //Create new ID
+      if (allTodos.length > 0) {
+        ID = allTodos[allTodos.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new todo
+      newTodo = new Todo(ID, description);
+
+      // Push new todo into array
+      allTodos.push(newTodo);
+
+      // Return the new object
+      return newTodo;
+    },
+
+    testing: function () {
+      console.log(allTodos);
+    }
+
+  };
 })();
 
 
@@ -21,6 +51,7 @@ var UIController = (function () {
   var classNames = {
     todoInputText: '.add-todo-text',
     todoInputBtn: '.add-todo-button',
+    todoList: '.todo-list'
   };
 
   // Global
@@ -28,13 +59,28 @@ var UIController = (function () {
     // Get the input from text field and return it as an object
     getInput: function () {
       return {
-        todo: document.querySelector(classNames.todoInputText).value
+        todoDescription: document.querySelector(classNames.todoInputText).value
       };
     },
+
+    addTodo: function (todoObj) {
+      // Get the UL for todo list container
+      var todoList = document.querySelector(classNames.todoList);
+
+      // Add the todo object description into the list
+      todoList.insertAdjacentHTML('afterend', `<li>${todoObj.description}</li>`);
+    },
+
     // Makes the class Names function public so the Global controller can access
     getClassNames: function () {
       return classNames;
-    }
+    },
+    // Clears the input text field
+    clearField: function () {
+      var field = document.querySelector(classNames.todoInputText);
+      field.value = ""; // Sets the field to blank
+      field.focus(); // Puts the focus back on the input field
+    },
   };
 })();
 
@@ -42,30 +88,52 @@ var UIController = (function () {
 // GLOBAL Controller
 var controller = (function (todoCtrl, UIctrl) {
 
-  var classNames = UIctrl.getClassNames(); // 
+  // Function to hold all event listeners - will be run in init()
+  var setupEventListeners = function () {
+    // Gives access to the classnames in UI controller
+    var classNames = UIctrl.getClassNames();
+
+    // Add item function will run if submit button is clicked
+    document.querySelector(classNames.todoInputBtn).addEventListener('click', ctrlAddItem);
+
+    // Add item function will run if enter (key13) is pressed
+    document.addEventListener('keypress', function (e) {
+      if (e.keyCode === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
   ctrlAddItem = function () {
+    var input, newTodo;
+
     // 1. Input value received from the getInput function in UIController
-    var input = UIctrl.getInput();
-    console.log(input);
-
-    // 2. Add the item to the todo controller
-
-    // 3. Add the item to the ui controller
-  }
-
-  // Add item function will run if submit button is clicked
-  document.querySelector(classNames.todoInputBtn).addEventListener('click', ctrlAddItem);
-
-  // Add item function will run if enter (key13) is pressed
-  document.addEventListener('keypress', function (e) {
-    if (e.keyCode === 13) {
-      ctrlAddItem();
+    input = UIctrl.getInput();
+    if (input.todoDescription !== "") {
+      // 2. Add the item to the todo controller
+      newTodo = todoCtrl.addTodo(input.todoDescription);
+      // 3. Add the item to the ui controller
+      UIctrl.addTodo(newTodo);
+      // 4. Clear the text input field
+      UIctrl.clearField();
     }
-  });
+  };
+
+  return {
+    // Init function to run any locally scoped functions in controller
+    init: function () {
+      console.log("App has started");
+      setupEventListeners();
+    }
+  };
+
+
 
 })(todoController, UIController);
 /* Here I am passing the todoController and UIController as two
 parameters to the controller function. This enables me to
 use the public methods from those two modules in this controller
 function. */
+
+// The init function is placed outside all the IIFE's so it can start up the controller on load.
+controller.init();
